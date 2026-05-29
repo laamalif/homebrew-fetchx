29cc72e523dd68ea89b21ffe52e6871a935c22a4
laamalif <11739379+laamalif@users.noreply.github.com>
2026-05-29 16:08:13 +0000

    fetchx: add 14.0_1 bottle.

--- Formula/fetchx.rb
+++ Formula/fetchx.rb
@@ -7,6 +7,13 @@ class Fetchx < Formula
   license all_of: ["BSD-2-Clause", "BSD-3-Clause"]
   revision 1
 
+  bottle do
+    root_url "https://github.com/laamalif/homebrew-fetchx/releases/download/fetchx-14.0_1"
+    sha256 cellar: :any, arm64_tahoe:  "4fdeb2259d9ea158350892180d89d71da7013fa6f90346ee9a8356072419ac8b"
+    sha256 cellar: :any, sequoia:      "69437866a545caa296e194b44b2b87b334e0d2252fb6a835b5de4bc848e0162a"
+    sha256 cellar: :any, x86_64_linux: "07b5cf1400b5ed2c53cfc15b33f2172e43aab5d5ec880ef28d8550abde3b7027"
+  end
+
   depends_on "cmake" => :build
   depends_on "openssl@3"
 
